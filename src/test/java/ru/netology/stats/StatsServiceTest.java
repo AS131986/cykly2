6a98cb3fe00b9ef7e4258139733364b4a2e77f94
Alexander Sklyar <AlexanderSGSklyar@mail.ru>
2022-08-16 19:25:22 +0300

    First

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,65 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalcSumAllSales() {
+        StatsService service = new StatsService();
+        int[] allSales = service.sales;
+        int expected = 180;
+
+        int actual = service.sumAllSales(allSales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcAverageSales() {
+        StatsService service = new StatsService();
+        int[] allSales = service.sales;
+        int expected = 15;
+        int actual = service.averageSales(allSales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcMaxSales() {
+        StatsService service = new StatsService();
+        int[] allSales = service.sales;
+        int expected = 5;
+        int actual = service.maxSalesMonth(allSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcLessAverage() {
+        StatsService service = new StatsService();
+        int[] allSales = service.sales;
+        int expected = 5;
+        int actual = service.maxSalesMonth(allSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcMoreAverage() {
+        StatsService service = new StatsService();
+        int[] allSales = service.sales;
+        int expected = 5;
+        int actual = service.maxSalesMonth(allSales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalcMinSales() {
+        StatsService service = new StatsService();
+        int[] allSales = service.sales;
+        int expected = 8;
+        int actual = service.minSalesMonth(allSales);
+        assertEquals(expected, actual);
+    }
+}
